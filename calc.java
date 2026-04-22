b0948bb3c09f8be313315060fa51783e52e74830
Adityaa0611 <aditya06rajput11@gmail.com>
2026-04-22 23:42:07 +0530

    Update calc.java

--- calc.java
+++ calc.java
@@ -20,7 +20,7 @@ public class calc {
     public static void main(String[] args) {
         calc c = new calc();
 
-        System.out.println("Addition: " + c.add(10, 5));
+        System.out.println("Addition: " + c.add(10, 65));
         System.out.println("Subtraction: " + c.sub(10, 5));
         System.out.println("Multiplication: " + c.mul(10, 5));
         System.out.println("Division: " + c.div(10, 5));
